f081f30d8ba8e66714a13d5a3f6663069e8044c7
MarshPeter <peter.m.farmer.204@gmail.com>
2024-06-21 00:03:14 +1000

    login page complete

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,3 +1,91 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./components/ui/form";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+
+const loginSchema = z.object({
+    username: z
+        .string()
+        .min(8, {
+            message: "Username must be at least 8 characters",
+        })
+        .max(40, {
+            message: "Username cannot be more than 40 characters",
+        }),
+    password: z
+        .string()
+        .min(8, {
+            message: "Password must be at least 8 characters",
+        })
+        .max(40, {
+            message: "Password cannot be more than 8 characters",
+        }),
+});
+
 export default function Login() {
-    return <div>Login</div>;
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof loginSchema>) {
+        console.log(values);
+    }
+    return (
+        <main className="p-4">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-4"
+                >
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input {...field}></Input>
+                                </FormControl>
+                                <FormDescription>
+                                    This is your display name
+                                </FormDescription>
+                                <FormMessage></FormMessage>
+                            </FormItem>
+                        )}
+                    ></FormField>
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input {...field}></Input>
+                                </FormControl>
+                                <FormDescription>
+                                    This is your associated password
+                                </FormDescription>
+                                <FormMessage></FormMessage>
+                            </FormItem>
+                        )}
+                    ></FormField>
+                    <Button>Login</Button>
+                </form>
+            </Form>
+        </main>
+    );
 }
