b46f866f4444924f59382f070847d6588e783e01
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-07-02 22:59:28 +1000

    refactored active pokemon cards into their own component

--- src/PokemonParty.tsx
+++ src/PokemonParty.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
+import ActivePokemon from "./components/PokemonParty/ActivePokemon";
 
 interface PokemonBrief {
     id: number;
@@ -65,44 +66,21 @@ export default function PokemonParty() {
                 <h1 className="text-3xl font-bold">Brock</h1>
             </div>
             <div>
-                <h2>Active Pokemon Party</h2>
-                {pokemon
-                    .filter((p) => p.inParty)
-                    .map((p) => {
-                        return (
-                            <div className="flex gap-2 border-2 border-gray-300 rounded-lg shadow-lg">
-                                <div className="p-1">
-                                    <img
-                                        src={p.img}
-                                        alt={`Image of ${p.name}`}
-                                        className="h-full border-2"
-                                    />
-                                </div>
-                                <div>
-                                    <p>{p.name}</p>
-                                    <p>
-                                        Health: {p.currentHealth}/{p.maxHealth}
-                                    </p>
-                                    <p>Rank: {p.rank}</p>
-                                    <div className="flex gap-2 mb-2 mt-2">
-                                        <p
-                                            className={`bg-green-600 text-white font-bold pb-1 pt-1 pl-3 pr-3 block rounded-lg shadow-xl`}
-                                        >
-                                            {p.typeOne}
-                                        </p>
-                                        {p.typeTwo ? (
-                                            <p
-                                                className={`bg-green-600 text-white font-bold pb-1 pt-1 pl-3 pr-3 block rounded-lg shadow-xl`}
-                                            >
-                                                {p.typeTwo}
-                                            </p>
-                                        ) : null}
-                                    </div>
-                                    <Button className="mb-2">Bench</Button>
-                                </div>
-                            </div>
-                        );
-                    })}
+                <section className="pb-4">
+                    <h2>Active Pokemon Party</h2>
+                    <div className="flex flex-col gap-4">
+                        {pokemon
+                            .filter((p) => p.inParty)
+                            .map((p) => {
+                                return (
+                                    <ActivePokemon pokemon={p}></ActivePokemon>
+                                );
+                            })}
+                    </div>
+                </section>
+                <section>
+                    <h2>PC Pokemon</h2>
+                </section>
                 {/* <div className="flex gap-2 border-2 border-gray-300 rounded-lg shadow-lg">
                     <div className="p-1">
                         <img
--- src/components/PokemonParty/ActivePokemon.tsx
+++ src/components/PokemonParty/ActivePokemon.tsx
@@ -1,3 +1,56 @@
-export default function ActivePokemon() {
-    return <div>ActivePokemon</div>;
+import { Button } from "../ui/button";
+
+interface PokemonBrief {
+    id: number;
+    name: string;
+    maxHealth: number;
+    currentHealth: number;
+    rank: number;
+    typeOne: string;
+    typeTwo?: string;
+    inParty: boolean;
+    img: string;
+}
+
+interface Props {
+    pokemon: PokemonBrief;
+}
+
+export default function ActivePokemon({ pokemon }: Props) {
+    return (
+        <div className="flex gap-2 border-2 border-gray-300 rounded-lg shadow-lg">
+            <div className="p-1">
+                <img
+                    src={pokemon.img}
+                    alt={`Image of ${pokemon.name}`}
+                    className="h-full border-2"
+                />
+            </div>
+            <div className="w-full pr-3">
+                <p>{pokemon.name}</p>
+                <p>
+                    Health: {pokemon.currentHealth}/{pokemon.maxHealth}
+                </p>
+                <p>Rank: {pokemon.rank}</p>
+                <div className="flex gap-2 mb-2 mt-2">
+                    <p
+                        className={`bg-green-600 text-white font-bold pb-1 pt-1 pl-3 pr-3 block rounded-lg shadow-xl`}
+                    >
+                        {pokemon.typeOne}
+                    </p>
+                    {pokemon.typeTwo ? (
+                        <p
+                            className={`bg-green-600 text-white font-bold pb-1 pt-1 pl-3 pr-3 block rounded-lg shadow-xl`}
+                        >
+                            {pokemon.typeTwo}
+                        </p>
+                    ) : null}
+                </div>
+                <div className="flex justify-between">
+                    <Button>View</Button>
+                    <Button className="mb-2">Bench</Button>
+                </div>
+            </div>
+        </div>
+    );
 }
